10b520c38f043a546c164d7d7eefcc4a411ef2da
Jayanta <bjayanta.me@gmail.com>
2022-05-05 13:23:34 +0600

    update app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
+import Head from "next/head";
 import 'styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/layout.css';
 import Header from "components/Header";
 import Footer from "components/Footer";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />);
     
   }
+  
   return <>
     <Head>
       <title>Home page</title>
