632b255f1f8c9d0258a12ce7e176b0085ee562bb
Jayanta <bjayanta.me@gmail.com>
2022-05-05 13:46:19 +0600

    work with jsconfig.json file

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import Head from "next/head";
-import 'styles/globals.css'
+import 'styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/layout.css';
-import Header from "components/Header";
-import Footer from "components/Footer";
+import Header from "@/layout/Header";
+import Footer from "@/layout/Footer";
 
 function MyApp({ Component, pageProps }) {
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />);
-    
   }
   
   return <>
